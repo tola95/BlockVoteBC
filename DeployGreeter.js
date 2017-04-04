06874849c382781a46b08f69d4d88544cc6ba2d6
Omotola <omotola.babasola@yahoo.com>
2017-04-04 02:47:09 +0100

    Deploy Greeter

---
+++ DeployGreeter.js
@@ -0,0 +1,26 @@
+/**
+ * Created by OmotolaBabasola1 on 04/03/2017.
+ */
+
+var greeterSource = 'contract mortal { address owner; function mortal() { owner = msg.sender; } function kill() { if (msg.sender == owner) suicide(owner); } } contract greeter is mortal { string greeting; function greeter(string _greeting) public { greeting = _greeting; } function greet() constant returns (string) { return greeting; } }';
+
+var greeterCompiled = web3.eth.compile.solidity(greeterSource);
+
+var _greeting = "Hello World!";
+
+var greeterContract = web3.eth.contract(greeterCompiled["<stdin>:greeter"].info.abiDefinition);
+
+var greeter = greeterContract.new(_greeting,{from:web3.eth.accounts[0], data: greeterCompiled["<stdin>:greeter"].code, gas: 300000}, function(e, contract){
+    if(!e) {
+
+        if(!contract.address) {
+            console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
+
+        } else {
+            console.log("Contract mined! Address: " + contract.address);
+            console.log(contract);
+        }
+
+    }
+});
+
